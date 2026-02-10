a33a4b2e92f5f32516c638791ac844e03f497781
santa <santanaruse@gmail.com>
2026-02-10 22:51:08 +0900

    add td prj

---
+++ TD/src/set_motor_pos.py
@@ -0,0 +1,83 @@
+"""
+CHOP Execute DAT
+
+me - this DAT
+
+Make sure the corresponding toggle is enabled in the CHOP Execute DAT.
+"""
+
+def onOffToOn(channel: Channel, sampleIndex: int, val: float, 
+              prev: float):
+	"""
+	Called when a channel changes from 0 to non-zero.
+	
+	Args:
+		channel: The Channel object which has changed
+		sampleIndex: The index of the changed sample
+		val: The numeric value of the changed sample
+		prev: The previous sample value
+	"""
+
+	if channel.name == 'calib_mode':
+		op("serial1").send('F', terminator='\n')
+	
+	if channel.name == 'zero':
+		op("serial1").send('Z', terminator='\n')
+	return
+
+def whileOn(channel: Channel, sampleIndex: int, val: float, 
+            prev: float):
+	"""
+	Called every frame while a channel is non-zero.
+	
+	Args:
+		channel: The Channel object which has changed
+		sampleIndex: The index of the changed sample
+		val: The numeric value of the changed sample
+		prev: The previous sample value
+	"""
+	return
+
+def onOnToOff(channel: Channel, sampleIndex: int, val: float, 
+              prev: float):
+	"""
+	Called when a channel changes from non-zero to 0.
+	
+	Args:
+		channel: The Channel object which has changed
+		sampleIndex: The index of the changed sample
+		val: The numeric value of the changed sample
+		prev: The previous sample value
+	"""
+	if channel.name == 'calib_mode':
+		op("serial1").send('L', terminator='\n')
+	return
+
+def whileOff(channel: Channel, sampleIndex: int, val: float, 
+             prev: float):
+	"""
+	Called every frame while a channel is 0.
+	
+	Args:
+		channel: The Channel object which has changed
+		sampleIndex: The index of the changed sample
+		val: The numeric value of the changed sample
+		prev: The previous sample value
+	"""
+	return
+
+def onValueChange(channel: Channel, sampleIndex: int, val: float, 
+                  prev: float):
+	"""
+	Called when a channel value changes.
+	
+	Args:
+		channel: The Channel object which has changed
+		sampleIndex: The index of the changed sample
+		val: The numeric value of the changed sample
+		prev: The previous sample value
+	"""
+	if channel.name == 'pos':
+		cmd = 'P' + str(int(val))
+		op("serial1").send(cmd, terminator='\n')
+	return
