fb1c8721cc613b19967ede560aff250fe2cbd213
santa <santanaruse@gmail.com>
2026-02-18 11:24:01 +0900

    add serial sender

---
+++ TD/src/send_data2.py
@@ -0,0 +1,38 @@
+﻿"""
+CHOP Execute DAT
+
+me - this DAT
+
+Make sure the corresponding toggle is enabled in the CHOP Execute DAT.
+"""
+
+# --- 設定 ---
+# 実際のピクセルデータを持っているCHOPのパスを指定
+# (前回作成した Math CHOP などを指定します)
+DATA_SOURCE_CHOP = op('data2')
+
+# データを送信するSerial DATのパスを指定
+SERIAL_OP = op('serial3')
+
+# Arduinoと合わせるヘッダー
+HEADER = b'S'
+
+def onOffToOn(channel, sampleIndex, val, prev):
+	# データソースCHOPから全サンプルを取得
+	samples = DATA_SOURCE_CHOP[0].vals
+
+	# データの長さをチェック
+	if len(samples) != 27:
+		return
+
+	# データをバイトに変換
+	int_samples = [int(s) for s in samples]
+	data_bytes = bytes(int_samples)
+	message = HEADER + data_bytes
+
+	# シリアル送信
+	try:
+		SERIAL_OP.sendBytes(message)
+	except Exception as e:
+		print(f"Serial Send Error: {e}")
+	return
